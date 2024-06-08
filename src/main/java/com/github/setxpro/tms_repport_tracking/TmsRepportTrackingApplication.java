3e1017d46ff479c4987d367e444f1bda53a749a9
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-06-08 08:22:06 -0300

    initial commit

---
+++ src/main/java/com/github/setxpro/tms_repport_tracking/TmsRepportTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.github.setxpro.tms_repport_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsRepportTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsRepportTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/setxpro/tms_repport_tracking/TmsRepportTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.setxpro.tms_repport_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsRepportTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
